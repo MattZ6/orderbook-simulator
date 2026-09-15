d404c1db67956c9359285249134a56bd48033b69
Matheus Felipe Zanin <matt_z6@hotmail.com>
2026-09-15 14:17:51 -0300

    fix(layout): stack layout import

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,8 @@
 import "react-native-reanimated";
 
 import Feather from "@expo/vector-icons/Feather";
-import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { DarkTheme, Stack, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
